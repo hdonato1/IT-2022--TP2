f2b6ac48200290be86407653f596ca6582cdf59c
IT2022-TP1 <hdonato@pioix.edu.ar>
2022-06-08 08:09:08 -0300

    modificado

--- Punto-1-/src/main.cpp
+++ Punto-1-/src/main.cpp
@@ -1,84 +1,146 @@
-#include <Arduino.h>
-#include <WiFi.h>                    //Incluye la librería ESP8266WiFi
-const char ssid[] = "horacio";            //Definimos la SSDI de nuestro servidor WiFi -nombre de red- 
-const char password[] = "12345678";       //Definimos la contraseña de nuestro servidor 
-WiFiServer server(80);                    //Definimos el puerto de comunicaciones
-int LED_BUILTIN = 2;                           //Definimos el pin de salida - GPIO2 / D4
-int estado = !LOW;                         //Definimos la variable que va a recoger el estado del LED
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+#define LED_BUILTIN 2   
+
+//------------------------CODIGO HTML------------------------------
+String pagina = "<!DOCTYPE html>"
+"<!DOCTYPE html>"
+"<html lang='en'>"
+"<head>"
+"<meta charset='UTF-8'>"
+"<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+"<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+"<title>Document</title>"
+"<style>"
+"html {"
+"box-sizing: border-box;"
+"font-size: 62.5%; /* 1 rem = 10px */"
+"}"
+"*, *:before, *:after {"
+"box-sizing: inherit;"
+"}"
+"body {"
+"font-size: 1.6rem;"
+"line-height: 2;"
+"}"
+"div{"
+"width:100%;"
+"text-align: center;"
+"}"
+"h1{"
+"font-size: 6rem;"
+"margin: 0;"
+"}"
+"button{"
+"font-size: 2rem;"
+"}"
+"button {"
+"background-color:#4eb5f1;"
+"color: #fff;"
+"border:none;" 
+"border-radius:10px;"
+"min-width: 120px;"
+"height: 50px;"
+"width: 100px;"
+"}"
+"/*medida de celular*/"
+"@media(max-width: 780px){"
+"h1{"
+"font-size: 10rem;"
+"}"
+"button{"
+"height: 80px;"
+"font-size: 4rem;"
+"}"
+"}"
+"</style>"
+"</head>"
+"<body>"
+"<div>"
+"<h1>Led</h1>"
+"<nav>"
+"<a href=\"/H\"><button >ON</button></a>"
+"<a href=\"/L\"><button >OFF</button></a>"
+"</nav>"
+"</div>"      
+"</body>"
+"</html>";
+
+//-------------------------------------------------------
+
+
+const char *ssid = "Horacio";
+const char *password = "123456789";
+
+WiFiServer server(80);
+
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);                //Inicializamos el GPIO2 como salida
-  digitalWrite(LED_BUILTIN, !LOW);              //Dejamos inicialmente el GPIO2 apagado
-  server.begin();                         //inicializamos el servidor
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);            //Red con clave, en el canal 1 y visible
-  //WiFi.softAP(ssid, password,3,1);      //Red con clave, en el canal 3 y visible 
-  //WiFi.softAP(ssid);                    //Red abierta
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
   Serial.println();
-  Serial.print("Direccion IP Access Point - por defecto: ");      //Imprime la dirección IP
-  Serial.println(WiFi.softAPIP()); 
-  Serial.print("Direccion MAC Access Point: ");                   //Imprime la dirección MAC
-  Serial.println(WiFi.softAPmacAddress()); 
-  //IPAddress local_ip(192, 168, 1, 1);                           //Modifica la dirección IP 
-  //IPAddress gateway(192, 168, 1, 1);   
-  //IPAddress subnet(255, 255, 255, 0);
-  //WiFi.softAPConfig(local_ip, gateway, subnet);
-  //Serial.println();
-  //Serial.print("Access Point - Nueva direccion IP: ");
-  //Serial.println(WiFi.softAPIP());
+  Serial.println("Configuring access point...");
+
+  // You can remove the password parameter if you want the AP to be open.
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+
+  Serial.println("Server started");
 }
-void loop() 
-{
-  // Comprueba si el cliente ha conectado
-  WiFiClient client = server.available();  
-  if (!client) {
-    return;
+
+void loop() {
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print(pagina);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
   }
-  // Espera hasta que el cliente envía alguna petición
-  Serial.println("nuevo cliente");
-  while(!client.available()){
-    delay(1);
-  }
-  // Imprime el número de clientes conectados
-  Serial.printf("Clientes conectados al Access Point: %dn", WiFi.softAPgetStationNum()); 
-  // Lee la petición
-  String peticion = client.readStringUntil('r');
-  Serial.println(peticion);
-  client.flush();
-  // Comprueba la petición
-  if (peticion.indexOf('/LED=ON') != -1) {
-    estado = !HIGH;
-  } 
-  if (peticion.indexOf('/LED=OFF') != -1){
-    estado = !LOW;
-  }
-  //Enciende o apaga el LED en función de la petición
-  digitalWrite(LED_BUILTIN, estado);
-  // Envía la página HTML de respuesta al cliente
-  client.println("HTTP/1.1 200 OK");
-  client.println("");                                     //No olvidar esta línea de separación
-  client.println("<!DOCTYPE HTML>");
-  client.println("<meta charset='UTF-8'>");
-  client.println("<html>");
-      //Imprime el estado del led
-  client.print("<h1>El LED está ahora:</h1>");                 
-  if(estado == !HIGH) {
-    client.print("<h1>ENCENDIDO</h1>");  
-  } else {
-    client.print("APAGADO</h1>");
-  }
-      //Se crean enlaces para modificar el estado del LED      
-  client.println("<button type='button' onClick=location.href='/LED=ON' style='margin:auto; background-color:green; color:#A9F5A9; padding:5px; border:2px solid black; width:200;'><h2> ENCENDER</h2> </button>");
-  client.println("<button type='button' onClick=location.href='/LED=OFF' style='margin:auto; background-color:red; color:#F6D8CE; padding:5px; border:2px solid black; width:200;'><h2> APAGAR</h2> </button><br><br>");
-      //Se crea un único botón para modificar el estado del LED
-  if(estado == !HIGH) {
-    client.print("<button type='button' onClick=location.href='/LED=OFF'> APAGAR </button><br><br>");  
-  } else {
-    client.print("<button type='button' onClick=location.href='/LED=ON'> ENCENDER </button><br><br>");
-  }
-  client.println("</html>"); 
-  delay(1);
-  Serial.println("Petición finalizada");          // Se finaliza la petición al cliente. Se inicaliza la espera de una nueva petición.
-  //Desconexión de los clientes
-  //WiFi.softAPdisconnect();
 }
